a41293ee4ad7dceffaad694ac77dc194754af532
caseyway <caseyway9999@gmail.com>
2020-09-28 22:49:38 -0700

    Add files via upload

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,80 @@
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
+ _____ ____ _____   ____ _____ ____
+| ____/ ___| ____| |___ \___  | ___|
+|  _|| |   |  _|     __) | / /|___ \
 | |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
+|_____\____|_____| |_____/_/  |____/
+
+ _   _                                         _      _
 | | | | ___  _ __ ___   _____      _____  _ __| | __ / |
 | |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
 |  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
 |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
 
+
+Aww Geez Man: Interdimensional Mortys.
 One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
 exists a Morty who says everything backwards.
-
 */
 
 // Include the Morty header file
 #include "Morty.hpp"
+#include <cstdlib>
 
+int main(int ac, char** av) {
+    // Parse the command line arguments. The program is executed as
+    // ./AwwGeezMan {start} {stop} {dimension}
+    // or 
+    // ./AwwGeezMan {start} {stop} {step} {dimension}
+    if (ac != 4 and ac != 5) {
 
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
-	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
-			<< std::endl;
-		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
-		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
-		return -1;
-	}
-	
-	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
-	return 0;
+        std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" << std::endl;
+        std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
+        std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
+
+        return -1;
+    }
+
+    // Parse the command line arguments
+
+    int start = atoi(av[1]);
+    int stop = atoi(av[2]);
+    int step;
+
+    std::string dimension;
+
+    if (ac == 4) {
+        dimension = av[ac - 1];
+    }
+    else {
+        step = atoi(av[3]);
+        dimension = av[ac - 1];
+    }
+
+    // Depending on the dimension of the arguments, call the appropriate Morty
+    // function
+
+    if (dimension == "Z286") {
+        if (step == 1) {
+            Z286::Morty(start, stop);
+        }
+        else {
+            Z286::Morty(start, stop, step);
+        }
+    }
+
+    if (dimension == "C137") {
+        if (step == 1) {
+            C137::Morty(start, stop);
+        }
+        else {
+            C137::Morty(start, stop, step);
+        }
+    }
+
+    if (dimension != "Z286" && dimension != "C137") {
+        std::cout << "ERROR: Unknown dimension!!" << std::endl;
+    }
+
+    return 0;
 }
--- src/Morty.cpp
+++ src/Morty.cpp
@@ -10,12 +10,90 @@
 // Include the Morty header file
 #include "Morty.hpp"
 
-// =========================================================================== // 
-//          Define functions enclosed in Namespace C137 here 
+// =========================================================================== //
+//          Define functions enclosed in Namespace C137 here
+// =========================================================================== //
+
+namespace C137 {
+    void Morty(int start, int stop, int step) {
+        std::cout<<"Morty C137 says:"<<std::endl;
+
+        for (int i = start; i <= stop; i = i + step) {
+            std::cout << i << ": ";
+
+            if (i % 3 == 0) {
+                std::cout << "Aww";
+            }
+            else if (i % 5 == 0) {
+                std::cout << "Geez";
+            }
+            else if (i % 15 == 0) {
+                std::cout << "Aww Geez Man";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void Morty(int start, int stop) {
+        std::cout << "Morty C137 says:" << std::endl;
+
+        for (int i = start; i <= stop; i = i++) {
+            std::cout << i << ": ";
+
+            if (i % 3 == 0) {
+                std::cout << "Aww";
+            }
+            else if (i % 5 == 0) {
+                std::cout << "Geez";
+            }
+            else if (i % 15 == 0) {
+                std::cout << "Aww Geez Man";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+// =========================================================================== //
+//          Define functions enclosed in Namespace Z286 here
 // =========================================================================== // 
 
+namespace Z286 {
+    void Morty(int start, int stop, int step) {
+        std::cout << "Morty Z286 says:" << std::endl;
+        
+        for (int i = start; i <= stop; i = i + step) {
+            std::cout << i << ": ";
 
+            if (i % 3 == 0) {
+                std::cout << "wwA";
+            }
+            else if (i % 5 == 0) {
+                std::cout << "zeeG";
+            }
+            else if (i % 15 == 0) {
+                std::cout << "naM zeeG wwA";
+            }
+            std::cout << std::endl;
+        }
+    }
 
-// =========================================================================== // 
-//          Define functions enclosed in Namespace Z286 here 
-// =========================================================================== // 
+    void Morty(int start, int stop) {
+        std::cout << "Morty Z286 says:" << std::endl;
+
+        for (int i = start; i <= stop; i = i++) {
+            std::cout << i << ": ";
+
+            if (i % 3 == 0) {
+                std::cout << "wwA";
+            }
+            else if (i % 5 == 0) {
+                std::cout << "zeeG";
+            }
+            else if (i % 15 == 0) {
+                std::cout << "naM zeeG wwA";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
